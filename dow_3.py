9c0cd1725b5f5c204e3af47c98b0cf76ef337b2c
Veronika Halamkova <veronikahalamkova@gmail.com>
2020-10-12 12:28:15 +0200

    renamed + sp500

--- dow_3.py
+++ dow_3.py
@@ -11,7 +11,7 @@ import numpy as np
 import os
 
 # %%
-pandas.set_option('display.max_rows', 10000)
+pd.set_option('display.max_rows', 10000)
 dow_list = si.tickers_dow()
 
 # %%
@@ -20,86 +20,33 @@ dow_list = si.tickers_dow()
 
 all_stock_dict = {}
 #all_stock_dict['dow']= 'hello world'
-for akcie in dow_list:
-    akcie_daily = get_data(akcie, start_date="01/01/2019", end_date="10/01/2020", index_as_date = True, interval="1d")
-    all_stock_dict[akcie]=akcie_daily
+for stock in dow_list:
+    stock_daily = get_data(stock, start_date="01/01/2019", end_date="10/01/2020", index_as_date = True, interval="1d")
+    all_stock_dict[stock]=stock_daily
 
 print (all_stock_dict)
 
 # %%
-for nazev_akcie, hodnota_akcie in all_stock_dict.items():
-  date_range = f"{str(hodnota_akcie.index[0])[:-9]}_{str(hodnota_akcie.index[-1])[:-9]}"
-  filename = f"{nazev_akcie}_{date_range}.csv"
+for stock_key, stock_value in all_stock_dict.items():
+  date_range = f"{str(stock_value.index[0])[:-9]}_{str(stock_value.index[-1])[:-9]}"
+  filename = f"{stock_key}_{date_range}.csv"
   full_filename = os.path.join ('output', filename)
   print(full_filename)
-  hodnota_akcie.to_csv(full_filename, index=True)
+  stock_value.to_csv(full_filename, index=True)
 
 # %%
-pokus = pd.read_csv(r'C:\Users\veron\DA\da_projekt\output\AAPL_2019-01-02_2020-09-30.csv')
+aapl = pd.read_csv(r'C:\Users\veron\DA\da_projekt\output\AAPL_2019-01-02_2020-09-30.csv')
+
+
+def rename(col):
+    if col.startswith("Unnamed: "):
+        return "date"
+    else:
+        return col
+
+aapl.columns = [rename(col) for col in aapl.columns]
 pokus.head()
 
 # %%
 all_stock_dict = {}
 all_stock_dict['dow']= 'hello world'
-
-# %%
-print(dow_list)
-# %%
-
-#Converting data into dictionary format
-all_stock_dict=[]
-for i in range (len(dow_list)):
-    all_stock_dict.append(all_stock[i].to_dict())
-    i +=1
-#print(all_stock_dict)
-#print(all_stock_dict[1])
-#print(all_stock_dict[1].values())
-#print(all_stock_dict[1].keys())
-#print(all_stock_dict[1].items())
-
-# %%
-
-# Getting data on stock industry
-industry_all = []
-i = 0
-for akcie in dow_list:
-    try:
-        Firma = yf.Ticker(akcie)
-        industry = Firma.info["sector"]
-    except Exception as e:
-        industry= "N/A"
-    finally: 
-        industry_all.append(industry)
-        i +=1
-print(industry_all)
-print(len(industry_all))
-
-# %%
-
-# Appending industry to stock data
-for i in range (len(dow_list)):
-    all_stock_dict[i] ["Ind"] = industry_all[i]
-
-#pandas.DataFrame.from_dict(all_stock_dict[1])
-
-# %%
-
-# Getting the dateframe of the stock dataset
-dateframe_all= []
-for i in range (len(dow_list)):
-    dateframe= f"{str(all_stock[i].index[0])[:-9]}_{str(all_stock[i].index[-1])[:-9]}"
-    dateframe_all.append(dateframe)
-    i +=1
-print (dateframe_all)
-
-# %%
-
-# Creating individual dictionary files for each stock 
-for i in range (len(dow_list)):
-    np.save (f"{dow_list[i]}_{dateframe_all[i]}.npy", all_stock_dict[i])
-
-#read_dictionary = np.load ('AXP.npy',allow_pickle='TRUE').item()
-#print (read_dictionary)
-
-# %%
-
--- sp500.py
+++ sp500.py
@@ -4,75 +4,53 @@
 import yahoo_fin
 from yahoo_fin.stock_info import get_data
 import yahoo_fin.stock_info as si
-import pandas
+import pandas as pd
 import yfinance as yf
 import traceback
 import numpy as np
-pandas.set_option('display.max_rows', 10000)
+import os
+
+# %%
+pd.set_option('display.max_rows', 10000)
 sp500_list = si.tickers_sp500()
 
 # %%
 
 #getting data from Yahoo Finance
-all_stock = []
-for akcie in sp500_list:
-    akcie_daily = get_data(akcie, start_date="01/01/2019", end_date="10/01/2020", index_as_date = True, interval="1d")
-    all_stock.append(akcie_daily)
-    print (all_stock)
 
-# %%
-
-#Converting data into dictionary format
-all_stock_dict=[]
-for i in range (len(sp500_list)):
-    all_stock_dict.append(all_stock[i].to_dict())
-    i +=1
-#print(all_stock_dict)
-#print(all_stock_dict[1])
-#print(all_stock_dict[1].values())
-#print(all_stock_dict[1].keys())
-#print(all_stock_dict[1].items())
-
-# %%
-
-# Getting data on stock industry
-industry_all = []
-i = 0
-for akcie in sp500_list:
+all_stock_dict = {}
+#all_stock_dict['dow']= 'hello world'
+for stock in sp500_list:
     try:
-        Firma = yf.Ticker(akcie)
-        industry = Firma.info["sector"]
-    except Exception as e:
-        industry= "N/A"
-    finally: 
-        industry_all.append(industry)
-        i +=1
-print(industry_all)
-print(len(industry_all))
+      stock_daily = get_data(stock, start_date="01/01/2019", end_date="10/01/2020", index_as_date = True, interval="1d")
+      all_stock_dict[stock]=stock_daily
+    except KeyError:
+      continue
+
+
+print (all_stock_dict)
 
 # %%
-
-# Appending industry to stock data
-for i in range (len(sp500_list)):
-    all_stock_dict[i] ["Ind"] = industry_all[i]
-
-#pandas.DataFrame.from_dict(all_stock_dict[1])
-
-# %%
-
-# Getting the dateframe of the stock dataset
-dateframe_all= []
-for i in range (len(sp500_list)):
-    dateframe= f"{str(all_stock[i].index[0])[:-9]}_{str(all_stock[i].index[-1])[:-9]}"
-    dateframe_all.append(dateframe)
-    i +=1
-print (dateframe_all)
+for stock_key, stock_value in all_stock_dict.items():
+  date_range = f"{str(stock_value.index[0])[:-9]}_{str(stock_value.index[-1])[:-9]}"
+  filename = f"{stock_key}_{date_range}.csv"
+  full_filename = os.path.join ('output', filename)
+  print(full_filename)
+  stock_value.to_csv(full_filename, index=True)
 
 # %%
+aapl = pd.read_csv(r'C:\Users\veron\DA\da_projekt\output\AAPL_2019-01-02_2020-09-30.csv')
 
-# Creating individual dictionary files for each stock 
-for i in range (len(sp500_list)):
-    np.save (f"{sp500_list[i]}_{dateframe_all[i]}.npy", all_stock_dict[i])
 
-#read_dictionary = np.load ('AXP.npy',allow_pickle='TRUE').item()
-#print (read_dictionary)
+def rename(col):
+    if col.startswith("Unnamed: "):
+        return "date"
+    else:
+        return col
+
+aapl.columns = [rename(col) for col in aapl.columns]
+pokus.head()
+
+# %%
+all_stock_dict = {}
+all_stock_dict['dow']= 'hello world'
