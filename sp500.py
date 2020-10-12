0fe0f8ef984abc57abbd069f232264a97ac8e404
Veronika Halamkova <veronikahalamkova@gmail.com>
2020-10-12 12:56:05 +0200

    fixed sp500

--- sp500.py
+++ sp500.py
@@ -13,19 +13,23 @@ import os
 # %%
 pd.set_option('display.max_rows', 10000)
 sp500_list = si.tickers_sp500()
+#print(sp500_list)
+len(sp500_list)
 
 # %%
 
 #getting data from Yahoo Finance
 
 all_stock_dict = {}
-#all_stock_dict['dow']= 'hello world'
+#all_stock_dict['sp500']= 'hello world'
 for stock in sp500_list:
     try:
       stock_daily = get_data(stock, start_date="01/01/2019", end_date="10/01/2020", index_as_date = True, interval="1d")
       all_stock_dict[stock]=stock_daily
-    except KeyError:
-      continue
+    except Exception as e:
+        print (repr(e))
+    else:
+        print (akcie_daily)
 
 
 print (all_stock_dict)
@@ -34,12 +38,12 @@ print (all_stock_dict)
 for stock_key, stock_value in all_stock_dict.items():
   date_range = f"{str(stock_value.index[0])[:-9]}_{str(stock_value.index[-1])[:-9]}"
   filename = f"{stock_key}_{date_range}.csv"
-  full_filename = os.path.join ('output', filename)
+  full_filename = os.path.join ('output\sp500', filename)
   print(full_filename)
   stock_value.to_csv(full_filename, index=True)
 
 # %%
-aapl = pd.read_csv(r'C:\Users\veron\DA\da_projekt\output\AAPL_2019-01-02_2020-09-30.csv')
+amzn = pd.read_csv(r'C:\Users\veron\DA\da_projekt\output\sp500\AMZN_2019-01-02_2020-09-30.csv')
 
 
 def rename(col):
@@ -48,9 +52,6 @@ def rename(col):
     else:
         return col
 
-aapl.columns = [rename(col) for col in aapl.columns]
-pokus.head()
+amzn.columns = [rename(col) for col in amzn.columns]
+amzn.head()
 
-# %%
-all_stock_dict = {}
-all_stock_dict['dow']= 'hello world'
