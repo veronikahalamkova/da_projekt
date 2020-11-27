dfa10e775131692f8f73d2f881b3f112d664a8e2
Yveta Melounova <yvetamelounova@gmail.com>
2020-11-27 11:32:18 +0000

    revoking some git changes to fix git push error

--- Sentiment_table.py
+++ Sentiment_table.py
@@ -6,6 +6,15 @@ import glob
 from difflib import get_close_matches
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import numpy as np
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+from scipy.sparse import csr_matrix
+import time
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import sparse_dot_topn.sparse_dot_topn as ct
 
 # %%
 
@@ -151,26 +160,105 @@ for col in company_columns:
 
 df_news.head(20)
 
+# %%%
+
+def ngrams(string, n=3):
+    string = re.sub(r'[,-./]|\sBD',r'', string)
+    ngrams = zip(*[string[i:] for i in range(n)])
+    return [''.join(ngram) for ngram in ngrams]
+
+ngrams("Apple")
+
 # %%
-all_matches=[]
 
-for news in df_news["text"]:
-    for company in sp500_names["unofficial_name"]:
-        match = get_close_matches(news, company, n=1, cutoff=.4)
-        all_matches.append(match)
-  
+company_names = sp500_names["unofficial_name"]
+article_headlines=df_news["name"]
+vectorizer = TfidfVectorizer(min_df=1, analyzer=ngrams)
+tf_idf_matrix = vectorizer.fit_transform(article_headlines)
+query_vec=vectorizer.transform(company_names)
+
 # %%
 
-#news_company_dict=[]
+print(tf_idf_matrix[0])
 
-for article in df_news["text"]:
-    for company in sp500_names["unofficial_name"]:
-        if company in article:
-            #print(article)
-        else:
-            pass
+# %%
 
-#print(news_company_dict)
+results = cosine_similarity(tf_idf_matrix,query_vec)
+print(results)
 
+# %%
+
+def cossim_top_similarity(A, B, ntop, lower_bound=0):
+    # force A and B as a CSR matrix.
+    # If they have already been CSR, there is no overhead
+    A = A.tocsr()
+    B = B.tocsr()
+    M, _ = A.shape
+    _, N = B.shape
+ 
+    idx_dtype = np.int32
+ 
+    nnz_max = M*ntop
+ 
+    indptr = np.zeros(M+1, dtype=idx_dtype)
+    indices = np.zeros(nnz_max, dtype=idx_dtype)
+    data = np.zeros(nnz_max, dtype=A.dtype)
+
+    ct.sparse_dot_topn(
+        M, N, np.asarray(A.indptr, dtype=idx_dtype),
+        np.asarray(A.indices, dtype=idx_dtype),
+        A.data,
+        np.asarray(B.indptr, dtype=idx_dtype),
+        np.asarray(B.indices, dtype=idx_dtype),
+        B.data,
+        ntop,
+        lower_bound,
+        indptr, indices, data)
+
+    return csr_matrix((data,indices,indptr),shape=(M,N))
+
+# %%
+
+t1 = time.time()
+matches = cossim_top_similarity(tf_idf_matrix, query_vec, 10, 0.8)
+t = time.time()-t1
+
+# %%
+
+print("SELFTIMED:", t)
+
+# %%
+def get_matches_df(sparse_matrix, name_vector, top=100):
+    non_zeros = sparse_matrix.nonzero()
+    
+    sparserows = non_zeros[0]
+    sparsecols = non_zeros[1]
+    
+    if top:
+        nr_matches = top
+    else:
+        nr_matches = sparsecols.size
+    
+    left_side = np.empty([nr_matches], dtype=object)
+    right_side = np.empty([nr_matches], dtype=object)
+    similairity = np.zeros(nr_matches)
+    
+    for index in range(0, nr_matches):
+        left_side[index] = name_vector[sparserows[index]]
+        right_side[index] = name_vector[sparsecols[index]]
+        similairity[index] = sparse_matrix.data[index]
+    
+    return pd.DataFrame({'left_side': left_side,
+                          'right_side': right_side,
+                           'similairity': similairity})
+        
+
+# %%
+
+matches_df = get_matches_df(matches, company_names, top=100000)
+matches_df = matches_df[matches_df['similairity'] < 0.99999] # Remove all exact matches
+matches_df.sample(20)
+
+# %%
 
 # %%
